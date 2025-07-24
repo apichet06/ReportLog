8caa1afe5e55660904b58390208aed95e00620e0
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2025-07-24 20:48:13 +0700

    update 4rd

--- src/features/reportLog/pages/ReportLogPage.tsx
+++ src/features/reportLog/pages/ReportLogPage.tsx
@@ -4,12 +4,12 @@ import {
   type GridRenderCellParams,
   type GridRowSelectionModel,
 } from "@mui/x-data-grid";
-import Paper from "@mui/material/Paper";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import Grid from '@mui/material/Grid';
 import { useCallback, useEffect, useState, type SyntheticEvent } from "react";
 import reportLogService from "../service/reportlogService";
 import type { ReportLog } from "../types/reportlog";
@@ -23,8 +23,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { Dayjs } from "dayjs";
-import CircularProgress from '@mui/material/CircularProgress';
-import Stack from '@mui/material/Stack';
+
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
@@ -44,7 +43,8 @@ export default function ReportLogPage() {
 
   const [startDate, setStartDate] = useState<Dayjs | null>(null);
   const [endDate, setEndDate] = useState<Dayjs | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [tapData, setTapData] = useState('DUC');
+
   const [loadingDataGrid, setLoadnigDataGrid] = useState(false);
 
   const [data, SetData] = useState<ReportLog[]>();
@@ -53,42 +53,42 @@ export default function ReportLogPage() {
     {
       field: "no",
       headerName: "ID",
-      width: 70,
+
       align: "center",
-      headerAlign: "center",
+      headerAlign: "center", flex: 0.5,
       renderCell: (params: GridRenderCellParams) => {
         const sortedRowIds = params.api.getSortedRowIds();
         const rowIndex = sortedRowIds.indexOf(params.id);
         return fNumber(rowIndex + 1);
       },
     },
-    { field: "group_name", headerName: "GROUP NAME", width: 130 },
-    { field: "username", headerName: "USERNAME", width: 130 },
+    { field: "group_name", headerName: "GROUP NAME", flex: 2 },
+    { field: "username", headerName: "USERNAME", flex: 2 },
     {
       field: "action",
       headerName: "ACTION",
-      width: 90,
+
     },
     {
       field: "action_date_time",
       headerName: "ACTION DATE TIME",
       description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
+      sortable: false, flex: 2,
+
       renderCell: (params) => (datetime.DateTimeLongTH(params.row.action_date_time))
     },
-    { field: "detail", headerName: "DETAIL", width: 400 },
-    { field: "bu", headerName: "BU", width: 160 },
-    { field: "position", headerName: "POSITION", width: 160 },
+    { field: "detail", headerName: "DETAIL", flex: 5 },
+    { field: "bu", headerName: "BU", flex: 1, },
+    { field: "position", headerName: "POSITION", flex: 2, },
     {
-      field: "resigned_date", headerName: "RESINGNED DATE", width: 130,
+      field: "resigned_date", headerName: "RESINGNED DATE", flex: 2,
       renderCell: (params) => (params.row.resigned_date ? datetime.DateLongTH(params.row.resigned_date) : '')
     },
-    { field: "days_after_action", headerName: "DAY AFTER ACTION", width: 160 },
+    { field: "days_after_action", headerName: "DAY AFTER ACTION", flex: 2 },
     {
       field: "event_type",
-      headerName: "EVENT TYPE",
-      width: 140,
+      headerName: "EVENT TYPE", flex: 2,
+
       renderCell: (params) => {
         const even_type = params.value as string;
         return (
@@ -102,28 +102,28 @@ export default function ReportLogPage() {
     },
     {
       field: "download_more_10_files_day",
-      headerName: "DOWNLOAD MORE 10 FILES DAY",
-      width: 160,
+      headerName: "DOWNLOAD MORE 10 FILES DAY", flex: 2,
+
     },
     {
       field: "unauthorized",
-      headerName: "UNAUTHORIZED",
-      width: 140,
+      headerName: "UNAUTHORIZED", flex: 2,
+
     },
     {
       field: "employee_resigning_within_one_month",
-      headerName: "EMPLOYEE RESINGING WITHHIN ONE MONTH",
-      width: 160,
+      headerName: "EMPLOYEE RESINGING WITHHIN ONE MONTH", flex: 2,
+
     },
   ];
 
 
   const fetchDUC = useCallback(async () => {
     try {
-      setLoading(true)
+      // setLoading(true)
       const res = await reportLogService.GetReportLogService();
       SetData(res.data.result);
-      setLoading(false)
+      // setLoading(false)
     } catch (err) {
       console.log(err);
     }
@@ -178,13 +178,12 @@ export default function ReportLogPage() {
 
 
 
-
   useEffect(() => {
     fetchDUC();
   }, [fetchDUC]);
 
 
-  const paginationModel = { page: 0, pageSize: 5 };
+  const paginationModel = { page: 0, pageSize: 10 };
 
   const hendleSubmit = async () => {
     const rawData = Array.isArray(selectionModel)
@@ -227,26 +226,11 @@ export default function ReportLogPage() {
 
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 2 }}>
-      <Grid container spacing={2} justifyContent="center">
-        {loading ?
-          <Stack spacing={2} sx={{ color: 'grey.500' }} direction="row" alignItems="center">
-            <CircularProgress color="inherit" size={200} thickness={2} />
-          </Stack>
-          :
-          <Grid size={{ xs: 12 }}>
-            <Box sx={{ width: '100%', typography: 'body1' }}>
-              <TabContext value={value}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                  <TabList onChange={handleChange} aria-label="lab API tabs example">
-                    <Tab label="DUC Log" value="1" />
-                    <Tab label="DCC" value="2" />
-                  </TabList>
-                </Box>
-                <TabPanel value="1">Item One</TabPanel>
-                <TabPanel value="2">Item Two</TabPanel>
-              </TabContext>
-            </Box>
+    <>
+      <Container disableGutters maxWidth={false}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid size={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }} mt={2} mb={2}>
+
             <Box
               component="form"
               sx={{ "& .MuiTextField-root": { width: "100%" } }}
@@ -321,55 +305,93 @@ export default function ReportLogPage() {
                 justifyContent="start"
                 alignItems="start"
               >
-                <Grid size={{ xs: 11, sm: 11, md: 11, lg: 8 }} >
+                <Grid size={{ xs: 11, sm: 11, md: 11, lg: 8, xl: 8 }} >
                   <h2>Report Log DCC & DUC</h2>
                 </Grid>
-                <Grid size={{ xs: 1, sm: 1, md: 1, lg: 2 }} mt={3}>
+                <Grid size={{ xs: 1, sm: 1, md: 1, lg: 4, xl: 4 }} mt={3} justifyContent="flex-end" display="flex">
                   <Button variant="contained" color="success" onClick={hendleSubmit}>
                     <SaveIcon />
                     submit to save
                   </Button>
-                </Grid>
-                <Grid size={{ xs: 1, sm: 1, md: 1, lg: 2 }} mt={3}>
-                  <Button variant="outlined" startIcon={<SystemUpdateAltIcon />} onClick={handleExportExcel}>
+                  <Button variant="outlined" startIcon={<SystemUpdateAltIcon />} onClick={handleExportExcel} sx={{ ml: 2 }}>
                     Export Excel
                   </Button>
                 </Grid>
               </Grid>
-            </Box>
-            <Paper sx={{ width: "100%", overflowX: "auto" }}>
-              <DataGrid
-                getRowId={(row) => row.id.toString()}
-                loading={loadingDataGrid}
-                rows={data}
-                columns={columns}
-                initialState={{ pagination: { paginationModel } }}
-                pageSizeOptions={[5, 10, 20, 40, 60, 80, 100]}
-                checkboxSelection
-                onRowSelectionModelChange={(newSelection) =>
-                  setSelectionModel(newSelection)
-                }
-                getRowClassName={(params) =>
-                  params.row.unauthorized == "Y" ||
-                    params.row.download_more_10_files_day == "Y" ||
-                    params.row.employee_resigning_within_one_month == "Y"
-                    ? "row--highlight"
-                    : ""
-                }
-                sx={{
-                  "& .row--highlight": {
-                    bgcolor: "rgba(255,165,0,0.1)",
-                    color: "orange", // สีตัวอักษร
-                    "&:hover": { bgcolor: "rgba(0, 128, 0, 0.15)" }, // hover color
-                  },
-                  fontSize: "12px",
-                }}
-              />
-            </Paper>
+              <Grid size={12} >
+                <TabContext value={value}>
+                  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} textColor="secondary"
+                      indicatorColor="secondary">
+                      <Tab label="DUC Log" value="1" onClick={() => setTapData('DUC')} />
+                      <Tab label="DCC Log" value="2" onClick={() => setTapData('DCC')} />
+                    </TabList>
+                  </Box>
+                  <TabPanel value="1">
+                    <DataGrid
+                      getRowId={(row) => row.id.toString()}
+                      loading={loadingDataGrid}
+                      rows={data}
+                      columns={columns}
+                      initialState={{ pagination: { paginationModel } }}
+                      pageSizeOptions={[5, 10, 20, 40, 60, 80, 100]}
+                      checkboxSelection
+                      onRowSelectionModelChange={(newSelection) =>
+                        setSelectionModel(newSelection)
+                      }
+                      getRowClassName={(params) =>
+                        params.row.unauthorized === "Y" ||
+                          params.row.download_more_10_files_day === "Y" ||
+                          params.row.employee_resigning_within_one_month === "Y"
+                          ? "row--highlight"
+                          : ""
+                      }
+                      sx={{
+                        "& .row--highlight": {
+                          bgcolor: "rgba(255,165,0,0.1)",
+                          color: "orange",
+                          "&:hover": { bgcolor: "rgba(0, 128, 0, 0.15)" },
+                        },
+                        fontSize: "12px",
+                      }}
+                    />
+                  </TabPanel>
+                  <TabPanel value="2">
+                    <DataGrid
+                      getRowId={(row) => row.id.toString()}
+                      loading={loadingDataGrid}
+                      rows={data}
+                      columns={columns}
+                      initialState={{ pagination: { paginationModel } }}
+                      pageSizeOptions={[5, 10, 20, 40, 60, 80, 100]}
+                      checkboxSelection
+                      onRowSelectionModelChange={(newSelection) =>
+                        setSelectionModel(newSelection)
+                      }
+                      getRowClassName={(params) =>
+                        params.row.unauthorized === "Y" ||
+                          params.row.download_more_10_files_day === "Y" ||
+                          params.row.employee_resigning_within_one_month === "Y"
+                          ? "row--highlight"
+                          : ""
+                      }
+                      sx={{
+                        "& .row--highlight": {
+                          bgcolor: "rgba(255,165,0,0.1)",
+                          color: "orange",
+                          "&:hover": { bgcolor: "rgba(0, 128, 0, 0.15)" },
+                        },
+                        fontSize: "12px",
+                      }}
+                    />
+                  </TabPanel>
+                </TabContext>
 
+              </Grid>
+            </Box>
           </Grid>
-        }
-      </Grid>
-    </Container>
+        </Grid>
+      </Container >
+    </>
   );
 }
